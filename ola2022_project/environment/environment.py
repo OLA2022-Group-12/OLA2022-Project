9814843d4712f995cc6480ffe44e454e7d260352
RaulSingh-7 <raul.singh197@gmail.com>
2022-05-07 15:20:38 +0200

    Environment fixes to make it more consistent with the assignment

--- ola2022_project/environment/environment.py
+++ ola2022_project/environment/environment.py
@@ -6,7 +6,7 @@ They can all be tuned to make the environment behave differently
 '''
 
 # Probability of every class to show up. They should add up to 1
-class_probabilities = [0.3, 0.6, 0.1]
+class_ratios = [0.3, 0.6, 0.1]
 
 # Reservation price of every class
 reservation_prices = [10, 30, 20]
@@ -14,8 +14,19 @@ reservation_prices = [10, 30, 20]
 # Price of the 5 products
 product_prices = [10, 15, 25, 18, 5]
 
-# Dirichlet alpha variables for every class
-alpha_ratios = [[20, 10, 20, 20, 5, 30], [15, 25, 20, 5, 40, 15], [10, 20, 40, 5, 5, 20]]
+# Parameters of the alpha functions for every class. The functions sigmoidal.
+# The first value is the steepness, the second is the shift and the third is the
+# upper bound.
+parameters = [[0.17,4.5,50], [0.15,5,65], [0.22,5.3,100]]
+
+# Simple function of a straight line with a lower bound of 0. Argument budget is
+# an integer representing the budget. Argument params is a 2-element array
+# containing the 2 linear parameters.
+clicks = lambda budget, params: max(0, budget * params[0] + params[1])
+
+# The competitor budget is assumed to be constant, since the competitor is
+# non-strategic
+competitor_budget = 100
 
 # Lambda parameter, which is the probability of osserving the next secondary product
 # according to the project's assignment
@@ -29,6 +40,30 @@ np.random.seed(1337)
 
 # Products graph's matrix. It's a dummy matrix, should be initialized with populate_graphs
 graph = np.random.rand(5,5)
+
+
+def alpha_function(budget, steepness, shift, upper_bound):
+
+    '''Alpha function with the shape of a sigmoidal function. Computes the
+    expected value of clicks given a certain budget.
+
+    Arguments:
+        budget: integer or float representing the budget 
+        
+        steepness: float representing the steepness of the sigmoidal curve 
+        
+        shift: float representing the shift of the entire function. The shift 
+            should always be positive and not smaller than 3 circa 
+        
+        upper_bound: integer representing the maximum expected number of \
+            clicks possible
+
+    Returns:
+        A float representing the expected value of number of clicks for a
+        certain class function 
+    '''
+
+    return upper_bound * (1 / ( 1 + np.exp(-steepness*budget + shift) ))
 
 
 def populate_graph(size=5, fully_connected=True, zeros_probability=0.5):
@@ -64,18 +99,17 @@ def populate_graph(size=5, fully_connected=True, zeros_probability=0.5):
     # Removes auto-loops from graph
     np.fill_diagonal(graph, 0)
 
-    
 
-
-def get_interaction(alphas):
+def get_interaction(user_class, primary_product):
     '''Computes a single interaction and returns it.
 
     This method shouldn't be called on its own, but only via the
     get_day_of_interactions() method
 
     Arguments:
-        alphas: list of 3 array of 6 elements representing the alpha ratios for
-            all the three user classes
+        user_class: integer from 1 to 3 representing user's class
+        primary_product: integer from 0 to 5 representing product number. 
+            Note: product 0 is the competitor's
 
     Returns:
         A tuple, the first element is an integer (1, 2 or 3) representing the
@@ -84,34 +118,31 @@ def get_interaction(alphas):
         bought of the product i+1    
     '''
 
-    # User's class is chosen randomly with weighted probability
-    user_class = np.random.choice([1, 2, 3], p=class_probabilities)
-
     # This array is initialized with zeros and represents the quantity of bought
     # items for every product
     items_bought = np.zeros(5)
-
-    primary_product = np.random.choice(list(range(6)), p=alphas[user_class])
 
     # If primary_product != 0 it means that the user landed on a page of one of
     # the 5 products, otherwise the user landed on a competitor product's page
     if primary_product != 0:
         primary_product -= 1
-        go_to_page(primary_product, user_class, items_bought)
+        go_to_page(user_class, primary_product, items_bought)
 
     return user_class, items_bought
 
 
-def get_day_of_interactions(num_customers):
+def get_day_of_interactions(num_customers, budgets):
     
     ''' Main method to be called when interacting with the environment. Outputs
-    all the interactions of an entire day. When called discards the old alpha
-    values and generates new alphas from a new realization of the Dirichlet
-    distribution. If called again it will be considered a new day.
+    all the interactions of an entire day. When called generates new alphas from
+    the clicks function with budget as input.
 
     Arguments:
         num_customers: total number of customers that will make interactions in
             a day
+        
+        budgets: numpy array of 5 integers containing the budget for each
+            product. The i-element is the budget for the i+1 product
     
     Returns:
         A list of tuples, with a size corresponding to num_customers. Every
@@ -120,22 +151,44 @@ def get_day_of_interactions(num_customers):
         represents how many of the products i+1 the customer bought
     '''
 
-    # Alphas are initialized
-    alphas = list()
-    for i in range(3):
-        alphas.append(np.random.dirichlet(alpha_ratios[i]))
+    # Competitor budget is added to the array of budgets
+    budgets = np.insert(budgets, 0, competitor_budget)
+
+    # Computing total number of customers for each class based on class_ratios
+    customers_of_class_1 = int(num_customers*class_ratios[0])
+    customers_of_class_2 = int(num_customers*class_ratios[1])
+    customers_of_class_3 = num_customers - customers_of_class_1 - customers_of_class_2
+    customers_per_class = [customers_of_class_1, customers_of_class_2, customers_of_class_3]
 
     total_interactions = list()
 
-    # num_customers interactions are performed, then saved and returned
-    for i in range(num_customers):
-        user_class, items = get_interaction(alphas)
-        total_interactions.append((user_class, items))
+    # For every class, product ratios are computed
+    for i in range(3):
+        click_ratios = alpha_function(budgets, parameters[i][0], parameters[i][1], parameters[i][2])
+        alpha_ratios = np.random.dirichlet(click_ratios)
+        
+        # This array will contain how many customers will land on a certain product page
+        product_ratios = [int(alpha_ratios[0]*customers_per_class[i]),
+             int(alpha_ratios[1]*customers_per_class[i]),
+             int(alpha_ratios[2]*customers_per_class[i]),
+             int(alpha_ratios[3]*customers_per_class[i]),
+             int(alpha_ratios[4]*customers_per_class[i])]
+        product_ratios.append(customers_per_class[i] - sum(product_ratios))
 
+        # According to product ratios, for every product the computed number on
+        # users are landed on the right and the interaction starts
+        for idx, ratio in enumerate(product_ratios):
+            for interaction in range(ratio):
+                user_class, items = get_interaction(i, idx)
+                total_interactions.append((user_class, items))
+
+    # Shuffle the list to make data more realistic
+    np.random.shuffle(total_interactions)
     return total_interactions
 
 
 def go_to_page(user_class, primary_product, items_bought):
+
     '''Shouldn't be called from outside. Shows the user a page of the specified
     primary products.
     
@@ -164,10 +217,10 @@ def go_to_page(user_class, primary_product, items_bought):
         # redirected to a new primary product page where the secondary product
         # is the primary product
         if np.random.rand() < graph[primary_product, secondary_products[0]] and not items_bought[secondary_products[0]]:
-            go_to_page(secondary_products[0], items_bought)
+            go_to_page(user_class, secondary_products[0], items_bought)
         
         # When the user does not click on the secondary product in the first
         # slot if he observes and click on the second slot he gets redirected to
         # a new page where this secondary product is the primary product
         elif np.random.rand() < graph[primary_product, secondary_products[1]] * lam and not items_bought[secondary_products[1]]:
-            go_to_page(secondary_products[1], items_bought)
+            go_to_page(user_class, secondary_products[1], items_bought)
