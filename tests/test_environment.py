151b3ae17d7ae61abe889be824b5bfdbaf2d7eef
Ole Martin Ruud <dev@barskern.no>
2022-05-19 18:05:20 +0200

    Fix environment test naming

--- tests/test_environment.py
+++ tests/test_environment.py
@@ -7,42 +7,36 @@ from ola2022_project.environment.environment import alpha_function
 
 
 @given(
-    budget_fraction=st.floats(
-        allow_nan=False, allow_infinity=False, min_value=0.0, max_value=1.0
-    ),
+    budget=st.floats(allow_nan=False, allow_infinity=False, min_value=0.0),
     steepness=st.floats(allow_nan=False, allow_infinity=False, min_value=0.0),
     shift=st.floats(allow_nan=False, allow_infinity=False),
-    total_budget=st.floats(allow_nan=False, allow_infinity=False, min_value=0.0),
+    upper_bound=st.floats(allow_nan=False, allow_infinity=False, min_value=0.0),
 )
 @pytest.mark.filterwarnings("ignore:overflow encountered")
-def test_alpha_function_always_between_zero_and_total_budget(
-    budget_fraction, steepness, shift, total_budget
+def test_alpha_function_always_between_zero_and_upper_bound(
+    budget, steepness, shift, upper_bound
 ):
     # No matter the budget fraction between 0 and 1, the assigned budget/alpha
-    # value should always be between zero and the total budget (any non-zero value)
-    assert (
-        0.0
-        <= alpha_function(budget_fraction, steepness, shift, total_budget)
-        <= total_budget
-    )
+    # value should always be between zero and the upper bound (any non-zero value)
+    assert 0.0 <= alpha_function(budget, steepness, shift, upper_bound) <= upper_bound
 
 
 @given(
-    x_n=st.floats(allow_nan=False, allow_infinity=False, min_value=0.0, max_value=1.0),
-    x_m=st.floats(allow_nan=False, allow_infinity=False, min_value=0.0, max_value=1.0),
+    x_n=st.floats(allow_nan=False, allow_infinity=False, min_value=0.0),
+    x_m=st.floats(allow_nan=False, allow_infinity=False, min_value=0.0),
     steepness=st.floats(allow_nan=False, allow_infinity=False, min_value=0.0),
     shift=st.floats(allow_nan=False, allow_infinity=False),
-    total_budget=st.floats(allow_nan=False, allow_infinity=False, min_value=0.0),
+    upper_bound=st.floats(allow_nan=False, allow_infinity=False, min_value=0.0),
 )
 @pytest.mark.filterwarnings("ignore:overflow encountered")
-def test_alpha_function_is_increasing(x_n, x_m, steepness, shift, total_budget):
+def test_alpha_function_is_increasing(x_n, x_m, steepness, shift, upper_bound):
     # The alpha function should always be increasing, hence if a value of
-    # budget_fraction (x_0) is smaller than another (x_1), it should produce a
+    # budget (x_0) is smaller than another (x_1), it should produce a
     # lower or equal value the alpha function is called with the other
     # parameters fixed
     x_0 = np.minimum(x_n, x_m)
     x_1 = np.maximum(x_n, x_m)
 
-    assert alpha_function(x_0, steepness, shift, total_budget) <= alpha_function(
-        x_1, steepness, shift, total_budget
+    assert alpha_function(x_0, steepness, shift, upper_bound) <= alpha_function(
+        x_1, steepness, shift, upper_bound
     )
