265eae1d884969009b366ba87505ddd29750e793
Raul Singh <80096915+RaulSingh-7@users.noreply.github.com>
2022-09-16 14:51:01 +0200

    Update ola2022_project/environment/environment.py
    Co-authored-by: Davide Rigamonti <80167579+davide-rigamonti-polimi@users.noreply.github.com>

--- ola2022_project/environment/environment.py
+++ ola2022_project/environment/environment.py
@@ -36,11 +36,6 @@ AggregatedInteraction = namedtuple(
     "AggregatedInteraction", ["items_bought", "landed_on"]
 )
 
-# Similar to Interaction but doesn't cointain any reference to a user class
-AggregatedInteraction = namedtuple(
-    "AggregatedInteraction", ["items_bought", "landed_on"]
-)
-
 
 class Step(Enum):
 
