8885d3bc337d8d27a565f15f3e751f0e65bc0fc2
RaulSing-7 <raul.singh197@gmail.com>
2022-10-16 12:20:02 +0200

    Comments and general cleanup

--- ola2022_project/algorithms/reward_estimator.py
+++ ola2022_project/algorithms/reward_estimator.py
@@ -8,9 +8,34 @@ from ola2022_project.environment.environment import (
 from ola2022_project.optimization import budget_assignment
 
 
+"""WARNING: this module does not work 100% correctly! For some reason, the function
+find_optimal_superarm is flawed because in some condition doesn't the optimal superarm
+but a close one.
+"""
+
+
 def clairvoyant_reward(
     env: EnvironmentData, population: int, max_budget: int, n_budget_steps: int
 ):
+
+    """Computes the average reward playing the optimal superarm according
+    to the given parameters. It simulates a deterministic day using the optimal
+    superarm.
+
+    Arguments:
+        env: instance of EnvironmentData
+
+        population: int representing the total population
+
+        max_budget: maximum budget assignable according to the knapsack constraint
+
+        n_budget_steps: number of budget_steps
+
+    Returns:
+        An integer representing the expected reward while playing the optimal superarm.
+        This corresponds to the average optimal reward. Because of randomness, this reward
+        could be surpassed by a learner sometimes. This is perfectly normal.
+    """
 
     budget_steps = np.linspace(0, max_budget, n_budget_steps)
 
@@ -23,6 +48,7 @@ def clairvoyant_reward(
     units_sold = np.array(
         [interaction.items_bought for interaction in deterministic_day]
     )
+
     return np.sum(units_sold * env.product_prices) * np.mean([1, env.max_items])
 
 
@@ -35,7 +61,7 @@ def find_optimal_superarm(
     n_classes = len(env.class_ratios)
 
     budget_value = np.zeros((n_products, n_budget_steps))
-    multipliers = compute_reward_multiplier(env)
+    multipliers = _compute_reward_multiplier(env)
 
     for i in range(n_products):
         for j, budget in enumerate(budget_steps):
@@ -49,9 +75,14 @@ def find_optimal_superarm(
     return budget_assignment(budget_value)
 
 
-def compute_reward_multiplier(
+def _compute_reward_multiplier(
     env: EnvironmentData, population=1000, budget=[100, 100, 100, 100, 100]
 ):
+    """Computes reward multipliers for every product. Basically reward multiplier tells
+    how much profit a single product will bring on average, according to all the possible
+    paths on the graph. The parameters pupolation and budget are dummy parameters, they
+    are not relevant and do not influence the result.
+    """
 
     interactions = get_day_of_interactions(
         np.random.default_rng(), population, budget, env, deterministic=True
