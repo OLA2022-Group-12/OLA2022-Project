0f1c59dc6d8081c8dd575ccfab6336f7525d8568
Ole Martin Ruud <dev@barskern.no>
2022-11-27 02:04:42 +0100

    Implement three degrees separation graph influence

--- ola2022_project/optimization/graph_influence.py
+++ ola2022_project/optimization/graph_influence.py
@@ -1,7 +1,80 @@
-from typing import List, Tuple
+import logging
+from typing import List, Tuple, Optional
 
 import numpy as np
 from numpy.typing import NDArray
+
+
+logger = logging.getLogger(__name__)
+
+
+def get_influence_of_seed(
+    graph: NDArray[np.float64],
+    seed: int,
+    graph_squared: Optional[NDArray[np.float64]] = None,
+    graph_cubed: Optional[NDArray[np.float64]] = None,
+) -> float:
+    """Get the influence of a node in a graph, i.e. the "amount" of nodes that
+    the graph will activate when the adjacency matrix contains the activation
+    weight/probability.
+
+    This function is specialized for the following criterions:
+        - Only consider neighbors within three degress of separation from the
+          seed
+        - A node can only be activated once
+        - A node cannot activate itself
+
+    Arguments:
+        graph: numpy matrix (NxN) with floats between 0.0 and 1.0 which
+        determine the weight/probability of activation
+
+        seed: integer between 0 and N-1 which determines the node to calculate
+        influence from
+
+        graph_squared: optionally provide numpy matrix (NxN) of the graph
+        squared, matrix above. This can be used as an optimization if the graph
+        is explored from several seeds.
+
+        graph_cubed: optionally provide numpy matrix (NxN) of the graph cubed,
+        matrix above. This can be used as an optimization if the graph is
+        explored from several seeds.
+    """
+    # Store a vector with 1 where a node is active (so only seed for now)
+    node_active = np.array([int(i == seed) for i in range(len(graph))])
+
+    # Probability to activate direct neighbors (not including seed)
+    direct_prob = graph[seed] * (1 - node_active)
+
+    # Update active nodes with the source nodes of the last step
+    node_active += np.where(direct_prob > 0.0, 1, 0)
+
+    # Probability to activate neighbors of direct neighbors.
+    # After taking the matrix power, the row of our seed is the probability to
+    # reach the node j from the start node. We multiply by (1 - node_active) to
+    # ensure that already active nodes don't contribute more than once.
+    second_graph = np.linalg.matrix_power(graph, 2) if graph_squared is None else graph_squared
+    second_indirect_prob = second_graph[seed] * (1 - node_active)
+
+    # Update active nodes with the source nodes of the last step
+    node_active += np.where(second_indirect_prob > 0.0, 1, 0)
+
+    # Probability to activate neighbors of neighbors of neighbors
+    third_graph = np.matmul(graph, second_graph) if graph_cubed is None else graph_cubed
+    third_indirect_prob = third_graph[seed] * (1 - node_active)
+
+    logger.debug(f"first_prop  = {direct_prob}")
+    logger.debug(f"second_prop = {second_indirect_prob}")
+    logger.debug(f"third_prop  = {third_indirect_prob}")
+
+    logger.debug(f"first_graph = {graph}")
+    logger.debug(f"second_graph = {second_graph}")
+    logger.debug(f"third_graph = {third_graph}")
+
+    return (
+        np.sum(direct_prob)
+        + np.sum(second_indirect_prob)
+        + np.sum(third_indirect_prob)
+    )
 
 
 def get_all_interaction_from_node(
--- tests/test_graph_influence.py
+++ tests/test_graph_influence.py
@@ -1,25 +1,175 @@
-from hypothesis import given
+import numpy as np
+from hypothesis import given, strategies as st
 
 from ola2022_project.environment.environment import EnvironmentData
 from ola2022_project.optimization import get_expected_value_per_node
 
+from ola2022_project.optimization.graph_influence import get_influence_of_seed
+
 from tests import generated_environment
 
 
+# @given(
+#    env=generated_environment(),
+# )
+# def test_expected_value_per_node_returns_boundend_output(env: EnvironmentData):
+#    user_class = 0
+#    result = get_expected_value_per_node(
+#        env.graph,
+#        env.product_prices,
+#        [p.reservation_price for p in env.classes_parameters[user_class]],
+#        env.next_products,
+#        env.lam,
+#    )
+#
+#    assert len(result) == len(env.product_prices), "equal as amount of products"
+#
+#    for item in result:
+#        assert 0.0 <= item <= sum(env.product_prices), "within maximum reward"
+
+
+@given(st.floats(min_value=0.0, max_value=1.0, allow_nan=False, allow_infinity=False))
+def test_single_direct_neighbor(factor):
+    graph = np.zeros((2, 2))
+
+    graph[0, 1] = factor
+
+    influence = get_influence_of_seed(graph, 0)
+
+    np.testing.assert_approx_equal(factor, influence)
+
+
+@given(st.floats(min_value=0.0, max_value=1.0, allow_nan=False, allow_infinity=False))
+def test_single_secondary_neighbors(factor):
+    graph = np.zeros((3, 3))
+
+    # Direct
+    graph[0, 1] = factor
+    # Secondary
+    graph[1, 2] = factor
+
+    influence = get_influence_of_seed(graph, 0)
+
+    np.testing.assert_approx_equal(factor + factor**2, influence)
+
+
+@given(st.floats(min_value=0.0, max_value=1.0, allow_nan=False, allow_infinity=False))
+def test_single_tertiary_neighbors(factor):
+    graph = np.zeros((4, 4))
+
+    # Direct
+    graph[0, 1] = factor
+
+    # Secondary
+    graph[1, 2] = factor
+
+    # Tertiary
+    graph[2, 3] = factor
+
+    influence = get_influence_of_seed(graph, 0)
+
+    np.testing.assert_approx_equal(factor + factor**2 + factor**3, influence)
+
+
+@given(st.floats(min_value=0.0, max_value=1.0, allow_nan=False, allow_infinity=False))
+def test_leaf_tertiary_neighbors(factor):
+    graph = np.zeros((5, 5))
+
+    # Direct
+    graph[0, 1] = factor
+
+    # Secondary
+    graph[1, 2] = factor
+
+    # Tertiary
+    graph[2, 3] = factor
+    graph[2, 4] = factor
+
+    influence = get_influence_of_seed(graph, 0)
+
+    np.testing.assert_approx_equal(factor + factor**2 + 2 * factor**3, influence)
+
+
 @given(
-    env=generated_environment(),
+    st.integers(min_value=2, max_value=10),
 )
-def test_expected_value_per_node_returns_boundend_output(env: EnvironmentData):
-    user_class = 0
-    result = get_expected_value_per_node(
-        env.graph,
-        env.product_prices,
-        [p.reservation_price for p in env.classes_parameters[user_class]],
-        env.next_products,
-        env.lam,
+def test_self_loops_give_no_influence(n):
+    graph = np.identity(n)
+    influence = get_influence_of_seed(graph, 0)
+    np.testing.assert_approx_equal(0.0, influence)
+
+
+@given(st.floats(min_value=0.0, max_value=1.0, allow_nan=False, allow_infinity=False))
+def test_single_direct_neighbor_circular(factor):
+    graph = np.zeros((2, 2))
+
+    graph[0, 1] = factor
+    graph[1, 0] = factor
+
+    influence = get_influence_of_seed(graph, 0)
+
+    np.testing.assert_approx_equal(factor, influence)
+
+
+@given(st.floats(min_value=0.0, max_value=1.0, allow_nan=False, allow_infinity=False))
+def test_two_direct_neighbor_circular(factor):
+    graph = np.zeros((3, 3))
+
+    # Direct
+    graph[0, 1] = factor
+    graph[0, 2] = factor
+
+    # Indirect (but shouldn't affect probability)
+    graph[1, 2] = factor
+
+    influence = get_influence_of_seed(graph, 0)
+
+    np.testing.assert_approx_equal(2 * factor, influence)
+
+
+@given(
+    st.integers(min_value=2, max_value=10),
+    st.floats(min_value=0.0, max_value=1.0, allow_nan=False, allow_infinity=False),
+)
+def test_arbitary_size_graph(n, factor):
+    graph = np.full((n, n), fill_value=factor)
+
+    n_direct_neighbors = n - 1
+    n_second_neighbors = max(0, (n - n_direct_neighbors - 1))
+    n_third_neighbors = max(0, (n - n_second_neighbors - n_direct_neighbors - 1))
+
+    influence = get_influence_of_seed(graph, 0)
+    np.testing.assert_approx_equal(
+        n_direct_neighbors * factor
+        + n_second_neighbors * factor**2
+        + n_third_neighbors * factor**3,
+        influence,
     )
 
-    assert len(result) == len(env.product_prices), "equal as amount of products"
 
-    for item in result:
-        assert 0.0 <= item <= sum(env.product_prices), "within maximum reward"
+@given(
+    st.integers(min_value=2, max_value=10),
+    st.floats(min_value=0.0, max_value=1.0, allow_nan=False, allow_infinity=False),
+)
+def test_cached_square_and_cubed_graphs(n, factor):
+    graph = np.full((n, n), fill_value=factor)
+
+    n_direct_neighbors = n - 1
+    n_second_neighbors = max(0, (n - n_direct_neighbors - 1))
+    n_third_neighbors = max(0, (n - n_second_neighbors - n_direct_neighbors - 1))
+
+    graph_squared = np.linalg.matrix_power(graph, 2)
+    graph_cubed = np.linalg.matrix_power(graph, 3)
+
+    influence = get_influence_of_seed(
+        graph,
+        0,
+        graph_squared=graph_squared,
+        graph_cubed=graph_cubed,
+    )
+    np.testing.assert_approx_equal(
+        n_direct_neighbors * factor
+        + n_second_neighbors * factor**2
+        + n_third_neighbors * factor**3,
+        influence,
+    )
